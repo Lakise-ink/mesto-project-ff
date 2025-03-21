76213a7faf54fcc341137243a9cd3342ee1e6c5f
Lakise <alexb773@yandex.ru>
2025-03-21 20:45:43 +0300

    1 итер

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+git@github.com:Lakise-ink/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,28 @@
-// @todo: Темплейт карточки
+const placesList = document.querySelector('.places__list');
 
-// @todo: DOM узлы
 
-// @todo: Функция создания карточки
+const deleteCard = (cardElement) => { 
+    cardElement.remove();
+};
 
-// @todo: Функция удаления карточки
 
-// @todo: Вывести карточки на страницу
+const createCard = (initialCard, deleteCard) => {
+const cardTemplate = document.querySelector("#card-template").content; 
+const cardElement = cardTemplate.querySelector(".card").cloneNode(true); 
+const cardImage = cardElement.querySelector(".card__image");
+cardImage.src = initialCard.link;
+cardImage.alt = initialCard.name;
+cardElement.querySelector(".card__title").textContent = initialCard.name;
+
+const deleteButton = cardElement.querySelector(".card__delete-button");
+deleteButton.addEventListener("click", () => deleteCard (cardElement));
+
+return cardElement;
+};
+
+
+
+initialCards.forEach(function (initialCard) {
+    const cardElement = createCard(initialCard, deleteCard);
+    placesList.append(cardElement);
+    });
